44db57a8748194eb5d277560ce881b1ebbee372d
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2019-04-11 22:10:26 +0000

    Read in more than one quote

--- src/components/HomeQuotes.jsx
+++ src/components/HomeQuotes.jsx
@@ -2,18 +2,32 @@
  * @Author: Kenneth Kwakye-Gyamfi
  * @Date: 2019-04-11 21:03:19
  * @Last Modified by: Kenneth Kwakye-Gyamfi
- * @Last Modified time: 2019-04-11 21:18:44
+ * @Last Modified time: 2019-04-11 22:09:43
  */
 import React from 'react';
 
+const quotes = [
+	{
+		text: 'No price is too high to pay for the privilege of owning yourself',
+		author: 'Friedrich Nietzsche'
+	},
+	{
+		text: 'Whosoever desires constant success must change his conduct with the times',
+		author: 'Niccolo Machiavelli'
+	},
+	{
+		text: 'Great things are done by a series of small things brought together',
+		author: 'Vincent Van Gogh'
+	}
+];
+
 function HomeQuotes() {
-	const quote = '"Great things are done by a series of small things brought together"';
-	const author = 'Vincent Van Gogh';
+	const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
 	return (
 		<div className="home-quote grid pad-grid">
-			<p className="home-quote__title">{quote}</p>
-			<p className="home-quote__quote">{author}</p>
+			<p className="home-quote__title">{`"${quote.text}"`}</p>
+			<p className="home-quote__quote">{quote.author}</p>
 		</div>
 	);
 }
