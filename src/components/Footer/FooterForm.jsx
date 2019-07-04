b60772b356e1fdb4cbcac7fbc2d3dffce55c1b7d
boanit <38143306+boanit@users.noreply.github.com>
2019-07-04 15:56:32 +0900

    Update FooterForm.jsx

--- src/components/Footer/FooterForm.jsx
+++ src/components/Footer/FooterForm.jsx
@@ -15,45 +15,13 @@ export default class FooterForm extends React.Component {
 
 	render() {
 		return (
-			<!--form
+			<form
 				action="#"
 				method="post"
 				name="contact"
 				className="footer-form"
 				data-netlify="true">
-				<input
-					id="name"
-					type="text"
-					name="name"
-					placeholder="Name"
-					value={this.state.name}
-					className="footer-form__input"
-					onChange={e => this.setState({ name: e.target.value })}
-				/>
-
-				<input
-					id="email"
-					name="email"
-					type="email"
-					className="footer-form__input"
-					value={this.state.email}
-					onChange={e => this.setState({ email: e.target.value })}
-					placeholder="Email address"
-				/>
-
-				<textarea
-					id="message"
-					name="message"
-					className="footer-form__input footer-form__textarea"
-					value={this.state.message}
-					onChange={e => this.setState({ message: e.target.value })}
-					placeholder="Your message"
-				/>
-
-				<button type="submit" className="footer-form__button">
-					Submit
-				</button>
-			</form-->
+			</form>
 		);
 	}
 }
